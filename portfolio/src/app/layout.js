f2ef68ab9f2d6cc42ef4514e505658485fa92203
oEnzoCastro <enzocastru@gmail.com>
2025-03-10 18:47:29 -0300

    First Commit: Create Next Project

---
+++ portfolio/src/app/layout.js
@@ -0,0 +1,13 @@
+export const metadata = {
+  title: 'Enzo Castro',
+  description: 'Portfolio Web - Enzo Castro',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      
+      <body>{children}</body>
+    </html>
+  )
+}
