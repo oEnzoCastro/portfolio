05e7a47c9821d082fad380c8bd06f3f658112a64
oEnzoCastro <enzocastru@gmail.com>
2025-03-23 23:34:00 -0300

    ADD: Technologies To JSON

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,41 @@
 import Technologies from "@/components/Technologies";
 
 export default function Home() {
+  const technologies = [
+    {
+      img: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/1200px-Unofficial_JavaScript_logo_2.svg.png",
+      name: "Javascript",
+      desc: "Frontend/Backend Language",
+    },
+    {
+      img: "https://blog.geekhunter.com.br/wp-content/uploads/2020/07/pngwing.com_.png",
+      name: "Java",
+      desc: "Backend Language",
+    },
+    {
+      img: "https://cdn.iconscout.com/icon/free/png-256/free-python-logo-icon-download-in-svg-png-gif-file-formats--technology-social-media-vol-5-pack-logos-icons-2945099.png?f=webp&w=256",
+      name: "Phyton",
+      desc: "Backend Language",
+    },
+    {
+      img: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png",
+      name: "React",
+      desc: "Frontend Frameword",
+    }
+  ];
+
   return (
     <div className="flex gap-5 justify-center mt-5 flex-wrap">
-      <Technologies img="https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/1200px-Unofficial_JavaScript_logo_2.svg.png" name="Javascript" desc="Frontend/Backend Language"/>
-      <Technologies img="https://blog.geekhunter.com.br/wp-content/uploads/2020/07/pngwing.com_.png" name="Java" desc="Backend Language"/>
-      <Technologies img="https://149860134.v2.pressablecdn.com/wp-content/uploads/pythoned.png" name="Phyton" desc="Backend Language"/>
+      {technologies.map((technologie) => {
+        return (
+          <Technologies
+            key={technologie.name}
+            img={technologie.img}
+            name={technologie.name}
+            desc={technologie.desc}
+          />
+        );
+      })}
     </div>
   );
 }
--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Technologies = ({ img, name, desc }) => {
   return (
     <div className="bg-neutral-800 hover:bg-neutral-900 w-75 h-20 flex items-center p-5 gap-5 rounded-md border border-neutral-600 hover:border-neutral-700 transition ease-linear">
-      <img src={img} className="h-10 bg-gray-100 rounded-md" />
+      <img src={img} className="h-10 bg-gray-800 rounded-md p-1" />
       <div className="">
         <h1 className="text-base text-neutral-50">{name}</h1>
         <p className="text-xs text-neutral-400">{desc}</p>
