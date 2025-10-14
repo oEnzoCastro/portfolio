bc802f1720531d05ef8c3cd23fa89197a052ba8f
oEnzoCastro <enzocastru@gmail.com>
2025-10-14 18:36:53 -0300

    Feat: Add CurvedLoop component and integrate into Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,52 @@
 import TextPressure from "./components/TextPressure";
 import Liquid from "./themes/Liquid";
+import CurvedLoop from "./ui/CurvedLoop.tsx/CurvedLoop";
 
 export default function Home() {
   return (
-    <div style={{ position: "relative", width: "100%", height: "100dvh", display: "flex", justifyContent: "center", alignItems: "center" }}>
+    <div style={{ position: "relative", minHeight: "100dvh" }}>
       <div
         style={{
-          position: "absolute",
           width: "100%",
-          height: "100%",
+          height: "100dvh",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          position: "relative",
+          zIndex: 1,
+        }}
+      >
+        <div className="w-1/2">
+          <TextPressure
+            text="Enzo Castro"
+            flex={false}
+            alpha={false}
+            stroke={false}
+            width={false}
+            weight={true}
+            italic={true}
+            textColor="#ffffff"
+            strokeColor="#ff0000"
+            minFontSize={12}
+          />
+        </div>
+      </div>
+
+        <CurvedLoop speed={0.5} marqueeText="-" curveAmount={0} />
+
+      <div className="h-dvh backdrop-blur-md"></div>
+
+      <div
+        style={{
+          position: "fixed",
+          top: 0,
+          left: 0,
+          width: "100vw",
+          height: "100vh",
           zIndex: -1,
         }}
       >
         <Liquid />
-      </div>
-      <div className="w-1/2">
-        <TextPressure
-          text="Enzo Castro"
-          flex={true}
-          alpha={false}
-          stroke={false}
-          width={true}
-          weight={true}
-          italic={true}
-          textColor="#ffffff"
-          strokeColor="#ff0000"
-          minFontSize={12}
-        />
       </div>
     </div>
   );
